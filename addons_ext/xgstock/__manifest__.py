0ff7ddf817025ba11834fbafd2491f948d7bfd3b
sockyjam@126.com <sockyjam@126.com>
2018-11-21 23:54:14 +0800

    modify tree stucture,

--- addons_ext/xgstock/__manifest__.py
+++ addons_ext/xgstock/__manifest__.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "XGOutPrd",
+    'name': "xgstock_base",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        XGStock function.
+        Such like product, warehouse, place, productclass, and so on.""",
 
     'description': """
-        Long description of module's purpose
+        XG Stock module 
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Liangli Xingang",
+    'website': "http://www.scllxg.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Inventory',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
---
+++ addons_ext/xgstock/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import xgin_prd, xgout_prd, xgstock_base, xgstock_report, xgstorage
+
+
